e3b620c619ab193dfc07113bf677bbbb342dd022
joshpson <jpearson846@gmail.com>
2018-05-24 14:36:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: {in: :allowed_categories}
+  validates :content, length: {minimum: 100}
+end
+
+def allowed_categories
+  ["Fiction", "Non-Fiction"]
 end
